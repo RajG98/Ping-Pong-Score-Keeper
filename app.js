228ec32381564282b794b68484230b67b7db0aba
RajG98 <76178523+RajG98@users.noreply.github.com>
2022-07-09 19:06:04 +0530

    add

---
+++ app.js
@@ -0,0 +1,49 @@
+const btn1 = document.querySelector('#btn1');
+const btn2 = document.querySelector('#btn2');
+const reset = document.querySelector('#btnR');
+const player1 = document.querySelector('#player1');
+const player2 = document.querySelector('#player2');
+
+let numDefault = 3;
+let num = numDefault;
+
+window.addEventListener('click', (e) => {
+    if (e.target.nodeName === 'SELECT') {
+        num = parseInt(e.target.value);
+    };
+})
+
+
+btn1.addEventListener('click', () => {
+    scoreKeeper(player1, player2);
+})
+btn2.addEventListener('click', () => {
+    scoreKeeper(player2, player1);
+})
+reset.addEventListener('click', () => {
+    btn1.disabled = false;
+    btn2.disabled = false;
+    btn1.classList.remove("disabled");
+    btn2.classList.remove("disabled");
+    player1.classList.remove("greenColor","redColor");
+    player2.classList.remove("greenColor","redColor");
+    player1.innerText = 0;
+    player2.innerText = 0;
+})
+
+
+const scoreKeeper = function (PSI, PSC) {
+    let increase = parseInt(PSI.innerText);
+    increase += 1;
+    PSI.innerText = increase;
+
+    if (num === increase) {
+        btn2.disabled = true;
+        btn1.disabled = true;
+        btn1.classList.add("disabled");
+        btn2.classList.add("disabled");
+        PSI.classList.add("greenColor");
+        PSC.classList.add("redColor");
+    }
+    
+}
